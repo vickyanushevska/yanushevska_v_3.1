38778cc96d1e89ecfb8569841aa50c5940d00069
vickyanushevska <107133031+vickyanushevska@users.noreply.github.com>
2022-06-10 21:14:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1
+{
+    class Example
+    {
+        public static void Main()
+        {
+            Shop shop = new Shop();
+            shop.main();
+        }
+    }
+}
